a5b790fe7939dc5db092f9d932841c10d466e479
daiahnadine <157860737+daiahnadine@users.noreply.github.com>
2024-06-10 21:31:10 +0800

    Registration Form

--- reg-form-activity/connect.php
+++ reg-form-activity/connect.php
@@ -3,15 +3,12 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Select the database to use
 $database_name = "FirstConnection";
 $sql = "USE $database_name";
 if ($conn->query($sql) === FALSE) {
